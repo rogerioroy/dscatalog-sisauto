7f4f42cb08ddef062bc38e52d329ee60184c5b21
Rogerio Vasconcelos <rogerio_roy@hotmail.com>
2022-03-18 15:22:13 -0300

    DTO

--- backend/src/main/java/com/sisauto/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/sisauto/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.sisauto.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sisauto.dscatalog.dto.CategoryDTO;
 import com.sisauto.dscatalog.entities.Category;
 import com.sisauto.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,9 @@ public class CategoryService {
 	private CategoryRepository repository; 
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 	
 }
